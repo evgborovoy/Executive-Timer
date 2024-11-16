8c924e610eafd34f6d86deffd83f84ec978ec7c0
Evgeniy <borovoyevgen@gmail.com>
2024-11-17 01:39:24 +0400

    Initial Commit

---
+++ Executive Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Executive Timer
+//
+//  Created by Evgeniy Borovoy on 11/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Executive Timer/Executive_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Executive_TimerApp.swift
+//  Executive Timer
+//
+//  Created by Evgeniy Borovoy on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Executive_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
